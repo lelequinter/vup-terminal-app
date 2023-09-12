f066ca02a47724f8dd339a4c58d44055d20c66b7
lelequinter <lele.quinter@gmail.com>
2023-09-12 11:36:52 -0500

    guardar los environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'vup-terminal-app',
+    appId: '1:345022906346:web:db30ffed26083d6a8e3c93',
+    storageBucket: 'vup-terminal-app.appspot.com',
+    apiKey: 'AIzaSyCbDW-7581AAuA53Ml4biDrZo4Ex-ggAm0',
+    authDomain: 'vup-terminal-app.firebaseapp.com',
+    messagingSenderId: '345022906346',
+  },
   apiUrl: '',
   production: true,
 };
