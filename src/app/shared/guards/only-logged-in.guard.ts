507960ab08c29d2dba22c083edbf2484f7162f81
lelequinter <lele.quinter@gmail.com>
2023-09-14 16:12:11 -0500

    crear el guard de only-logged-in

--- src/app/shared/guards/only-logged-in.guard.ts
+++ src/app/shared/guards/only-logged-in.guard.ts
@@ -3,13 +3,13 @@ import { Router } from '@angular/router';
 import { AuthService } from '@app/pages/users/services/auth.service';
 import { take, tap } from 'rxjs';
 
-export const authGuard = () => {
+export const onlyLoggedInGuard = () => {
   const router = inject(Router);
   const authSvc = inject(AuthService);
 
   return authSvc.userState$
   .pipe(
     take(1),
-    tap( (isLoggedIn) => (!!isLoggedIn ? router.navigate(['/user/sign-in']) : true))
+    tap( (isLoggedIn) => ( isLoggedIn ? true : router.navigate(['/user/sign-in'])))
   )
 };
