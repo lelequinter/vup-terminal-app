bd06e1817dcd365e98784705c7e86b0064471088
lelequinter <lele.quinter@gmail.com>
2023-09-14 10:31:51 -0500

    arreglar el mensaje de error de la contraseña

--- src/app/pages/users/services/auth.service.ts
+++ src/app/pages/users/services/auth.service.ts
@@ -1,15 +1,33 @@
 import { Injectable, inject } from '@angular/core';
-import { Auth, GoogleAuthProvider, authState, signInWithRedirect } from '@angular/fire/auth';
+import {
+  Auth,
+  User,
+  authState,
+  signInWithRedirect,
+  GoogleAuthProvider,
+  sendEmailVerification,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from '@angular/fire/auth';
+import { Router } from '@angular/router';
 import { Auth as AuthFire } from "@firebase/auth";
+
+interface ErrorResponse {
+  code: string;
+  message: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   private readonly auth = inject(Auth);
+  private readonly router = inject(Router);
   private readonly googleProvider = new GoogleAuthProvider();
 
-  constructor() { }
+  constructor() {
+    // this.signOut()
+  }
 
   get userState$(){
     return authState(this.auth as AuthFire);
@@ -21,5 +39,53 @@ export class AuthService {
     } catch (error) {
       console.log('Google login', error);
     }
+  }
+
+  async signUp( email: string, password: string ): Promise<void>{
+    try {
+      const { user } = await createUserWithEmailAndPassword(this.auth, email, password);
+      await this.sendEmailVerfication(user)
+      this.router.navigate(['/user/email-verification']);
+    } catch (error: unknown) {
+      const { code, message } = error as ErrorResponse;
+
+      console.log('Code :', code);
+      console.log('Message :', message);
+    }
+  }
+
+  async signIn( email: string, password: string ): Promise<void>{
+    try {
+      const { user } = await signInWithEmailAndPassword(this.auth, email, password);
+      console.log(user);
+      this.checkUserIsVerified(user);
+      // this.router.navigate(['/user/profile'])
+    } catch (error: unknown) {
+      const { code, message } = error as ErrorResponse;
+
+      console.log('Code :', code);
+      console.log('Message :', message);
+    }
+  }
+
+  async signOut(): Promise<void>{
+    try {
+      await this.auth.signOut();
+    } catch (error: unknown) {
+      console.log('error: ',error);
+    }
+  }
+
+  async sendEmailVerfication(user: User): Promise<void>{
+    try {
+      await sendEmailVerification(user);
+    } catch (error: unknown) {
+      console.log('error: ',error);
+    }
+  }
+
+  private checkUserIsVerified(user: User): boolean {
+
+    return true;
   }
 }
--- src/app/shared/auth-form/components/error-message/validator-message.ts
+++ src/app/shared/auth-form/components/error-message/validator-message.ts
@@ -5,7 +5,7 @@ interface ErrorMessage {
 const errorMessages: ErrorMessage = {
   required: 'This field is required.',
   pattern: 'Email must be valid.',
-  minLength: 'This field must be at least 5 characters long.'
+  minlength: 'This field must be at least 5 characters long.'
 };
 
 export function validatorErrorMessage(validatorName: string): string{
